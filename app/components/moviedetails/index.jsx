8b2cc95a955bbeeafe07907f8b3bf59e46410a23
Bayu Slamet <bayuslamet.xrpl1@gmail.com>
2026-02-19 13:50:47 +0700

    Fix Bug Routing

--- app/components/moviedetails/index.jsx
+++ app/components/moviedetails/index.jsx
@@ -1,5 +1,7 @@
+"use client";
+
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams } from "next/navigation";
 import './style.css';
 
 const MovieDetails = () => {
